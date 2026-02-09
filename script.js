c8186f56b64007cd12e04e0ef03586a11f7d1b8e
isma-danniel <isma.danniel1211@gmail.com>
2026-02-09 13:48:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -44,3 +44,22 @@ if(id){
     document.getElementById("p-price").innerText = product.price;
   }
 }
+
+const products = [
+  {id:1, name:"Heritage", price:"$499", img:"https://images.unsplash.com/photo-1523275335684-37898b6baf30"},
+  {id:2, name:"Classic", price:"$459", img:"https://images.unsplash.com/photo-1508685096489-7aacd43bd3b1"},
+  {id:3, name:"Royal", price:"$599", img:"https://images.unsplash.com/photo-1522312346375-d1a52e2b99b3"},
+  {id:4, name:"Elegance", price:"$529", img:"https://images.unsplash.com/photo-1598970434795-26d7a4cc7519"}
+];
+
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+
+if(id){
+  const product = products.find(p => p.id == id);
+  if(product){
+    document.getElementById("p-img").src = product.img;
+    document.getElementById("p-name").innerText = product.name;
+    document.getElementById("p-price").innerText = product.price;
+  }
+}
